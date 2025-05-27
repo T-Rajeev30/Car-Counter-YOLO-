a9e91b8de6490ae84a5ba8cfe34d2be06a3e2020
T-Rajeev30 <rajeevtiwariunder100@gmail.com>
2025-05-27 23:46:58 +0530

    All the files being downloaded and new files are formed

---
+++ WebCam/Car_Counter.py
@@ -0,0 +1,56 @@
+import cv2
+from ultralytics import YOLO
+import cvzone 
+import math
+
+## to check whether the file exist or not 
+#import os
+#print(os.path.exists("./Videos/bikes.mp4"))
+
+
+#cap = cv2.VideoCapture(0)
+#cap.set(3,1280)
+#cap.set(4,720)
+
+cap = cv2.VideoCapture("./Videos/cars.mp4")
+
+model = YOLO("../YoloWeights/yolov8l.pt")
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+while True:
+    success, img = cap.read()
+    results = model(img , stream=True)
+    for r in results: 
+        boxes = r.boxes
+        for box in boxes:
+            #bounding box
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1,y1,x2,y2 =  int(x1),int(y1),int(x2),int(y2) 
+            
+            #cv2.rectangle(img , (x1,y1 ) ,(x2 , y2) , (255 , 0 ,20) , 3)
+            w, h = x2-x1 , y2-y1 
+
+            #confidence 
+            conf = math.ceil((box.conf[0]*100))/100
+            print(conf)
+            
+            #class name 
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+            if currentClass == "cars" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 0.3:
+
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}' , (max(0 ,x1), max(35 , y1)) ,scale=0.6, thickness=1 , offset=3  )
+                cvzone.cornerRect(img,(x1 , y1 , w , h) , l=9 )
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1) 
